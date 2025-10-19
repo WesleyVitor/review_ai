ca27ddd9b7c25e0a8d83cffb899baed6913d3e09
WesleyVitor <wesleydemorais@outlook.com.br>
2025-10-19 12:13:51 -0300

    feat: create api and call agents

---
+++ api/main.py
@@ -0,0 +1,37 @@
+import tempfile
+
+from typing import Annotated
+
+from fastapi import FastAPI, Form, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+from agents_core.review_agent import ReviewAgent
+from agents_core.exercise_agent import ExerciseAgent
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou sua lista de domínios
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/review/")
+async def review(term: Annotated[str, Form()], file: UploadFile):
+    
+    review_agent = ReviewAgent()
+    exercise_agent = ExerciseAgent()
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
+        content = await file.read()
+        tmp.write(content)
+        tmp_path = tmp.name
+    
+    review_content = await review_agent.run(term, tmp_path)
+    
+    exercises_content = await exercise_agent.run(review_content)
+    
+    return {"term": term, "review_content": review_content, "exercises_content":exercises_content}
