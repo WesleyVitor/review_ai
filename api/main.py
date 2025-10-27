49bd13433c2bf5b3d5c1dd1cc510740864b11432
WesleyVitor <wesleydemorais@outlook.com.br>
2025-10-27 20:08:31 -0300

    feat: add review object when call endpoint

--- api/main.py
+++ api/main.py
@@ -65,21 +65,73 @@ async def auth_google(data: TokenModel):
 
     
 @app.post("/review/add")
-async def add_review(term: Annotated[str, Form()], file: UploadFile):
-    
-    review_agent = ReviewAgent()
-    exercise_agent = ExerciseAgent()
+async def add_review(term: Annotated[str, Form()], file: UploadFile, request: Request):
+    auth_header = request.headers.get("authorization")
 
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
-        content = await file.read()
-        tmp.write(content)
-        tmp_path = tmp.name
-    
-    review_content = await review_agent.run(term, tmp_path)
-    
-    exercises_content = await exercise_agent.run(review_content)
-    
-    return {"term": term, "review_content": review_content, "exercises_content":exercises_content}
+    if not auth_header:
+        return JSONResponse({"err":"Token invalid"}, 403)
+
+    try:
+         # Decodifica o token
+        token = auth_header.split()[1]
+        
+        decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        user_id = decoded.get("user")
+        #review_agent = ReviewAgent()
+        #exercise_agent = ExerciseAgent()
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
+            content = await file.read()
+            tmp.write(content)
+            tmp_path = tmp.name
+        
+        session = SessionLocal()
+        #review_content = await review_agent.run(term, tmp_path)
+        review_object = Review(
+            filename = file.filename,
+            s3_url = "#",
+            media_type=file.content_type,
+            review_type="Review",
+            user_id=user_id
+        )
+        session.add(review_object)
+        #exercises_content = await exercise_agent.run(review_content)
+        exercises_object = Review(
+            filename = file.filename,
+            s3_url = "#",
+            media_type=file.content_type,
+            review_type="Exercises",
+            user_id=user_id
+        )
+        session.add(exercises_object)
+        session.commit()
+        return JSONResponse({"ok": "Success"})
+    except jwt.ExpiredSignatureError:
+        return JSONResponse({"err":"Token expired"}, 401)
+    except jwt.InvalidTokenError:
+        return JSONResponse({"err":"Token invalid"}, 403)
+    #return {"term": term, "review_content": review_content, "exercises_content":exercises_content}
+    # # Temporary files for the PDFs
+    # tmp_review = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
+    # tmp_exercises = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
+
+    # styles = getSampleStyleSheet()
+
+    # # --- PDF Revisão ---
+    # doc_review = SimpleDocTemplate(tmp_review.name, pagesize=A4)
+    # story_review = []
+    # for line in review.split("\n"):
+    #     story_review.append(Paragraph(line, styles["Normal"]))
+    #     story_review.append(Spacer(1, 12))
+    # doc_review.build(story_review)
+
+    # # --- PDF Exercícios ---
+    # doc_exercises = SimpleDocTemplate(tmp_exercises.name, pagesize=A4)
+    # story_exercises = []
+    # for line in exercises.split("\n"):
+    #     story_exercises.append(Paragraph(line, styles["Normal"]))
+    #     story_exercises.append(Spacer(1, 12))
+    # doc_exercises.build(story_exercises)
 
 
 
@@ -97,7 +149,8 @@ async def list_review(request: Request):
         decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
         user_id = decoded.get("user")
         session = SessionLocal()
-        reviews = session.query(Review).filter_by(user_id=user_id).all()
+        reviews = session.query(Review).filter_by(user_id=user_id).order_by(Review.id.desc()).all()
+        
         result = [
             {
                 "id": r.id,
