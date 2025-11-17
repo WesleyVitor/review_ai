d7eed164e5a7956f2e86d9bea63c3ec5b5cde781
WesleyVitor <wesleydemorais@outlook.com.br>
2025-11-16 21:26:12 -0300

    deploy: resolving deploy frontend

--- review_ui/app/layout.js
+++ review_ui/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
-import { Providers } from "./provider";
+import { Providers } from "./Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
